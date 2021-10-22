2b6d6f70081642c883037007192142fe9b4c889b
Alan Watson <alphawhiskey82@yahoo.com>
2021-10-22 16:03:56 -0400

    Fix test code

--- napari_imaris_loader/_tests/test_reader.py
+++ napari_imaris_loader/_tests/test_reader.py
@@ -3,16 +3,16 @@ from napari_imaris_loader import napari_get_reader
 
 
 # tmp_path is a pytest fixture
-def test_reader(tmp_path):
+def test_reader(tmp_path='./brain_crop3.ims'):
     """An example of how you might test your plugin."""
 
     # Test whether we get a callable reader function
     #reader = napari_get_reader('/path/to/a/fake.ims')
-    reader = napari_get_reader('./brain_crop3.ims')
+    reader = napari_get_reader(tmp_path)
     assert callable(reader)
 
     # make sure we're delivering the right format
-    layer_data = reader(my_test_file)
+    layer_data = reader(tmp_path)
     assert isinstance(layer_data, list)
     assert isinstance(layer_data[0], tuple) and len(layer_data[0]) == 2
     assert isinstance(layer_data[0][0], list) and isinstance(layer_data[0][1], dict)
